b504517f7bf80274f7a22de8d55624042fb317aa
Amaniambele <158152290+Amaniambele@users.noreply.github.com>
2025-02-09 00:51:28 -0800

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~yc0WDZhL#ujB9YxlM4NAwE0jQIw7zxJ3WFSxoK8WPQ3rtGml1De0",
 PORT: process.env.PORT || "8000"
 };
